b38c7c5f9dceed137babb5946d72e644ef35f9fb
Eduardo Celada Ramonet <eduardocelada@gmail.com>
2014-01-27 21:34:27 -0700

    Primera Base para creación de DB

--- WiW/WiWApp/models.py
+++ WiW/WiWApp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Perfil(models.Model):
+	IdPerfil=models.AutoField(primary_key=True)
+	nombre_completo = models.CharField(max_length=70)
+	fdn=models.DateTimeField()
+	email=models.EmailField()
+	ciudad=models.CharField()
+	estado=models.CharField()
+	pais=models.CharField()
+	twitter=models.CharField(max_length=15)
+	facebookid=models.IntegerField()
+	passw=models.CharField()
+
+class Posts(models.Model):
+	idPost=models.AutoField(primary_key=True)
+	FechaPost=models.datetime.auto_now_add=True
+	IdPerfil=models.ForeignKey('Perfil')
+	urlImg=models.URLField()
+
+class Comentarios(models.Model):
+	idComentario=models.AutoField(primary_key=True)
+	Comentador=models.ForeignKey('Perfil')
+	Comentario=models.CharField()
+
+class Likes(models.Model):
+	idLike=models.AutoField()
+	idPost=models.ForeignKey('Posts')
+	IdPerfil=models.ForeignKey('Perfil')
+
+class Estilo(models.Model):
+	idEstilo=models.AutoField(primary_key=True)
+	Estilo=models.CharField()
+
+class Colores(models.Model):
+	idColores=models.AutoField(primary_key=True)
+	Colores=models.CharField()
+
+class EstiloPerfil(models.Model):
+	idRelacion=models.AutoField()
+	IdPerfil=ForeignKey('Perfil')
+	idEstilo=ForeignKey('Estilo')
+	idColores=ForeignKey('Colores')
+
+
+
